5bcefcc2d408c454a6f921c1ef27abb1d1b023d9
milon2000 <milakozlowska2000@gmail.com>
2022-05-02 16:19:38 +0200

    3.5

---
+++ 3.5_b_MilenaKozlowska.pas
@@ -0,0 +1,23 @@
+program NajwiekszaLiczba;
+var a, b, c : integer ;
+
+begin
+writeln('Podaj liczbe a: ');
+readln(a);
+writeln('Podaj liczbe b: ');
+readln(b);
+writeln('Podaj liczbe c: ');
+readln(c);
+
+if a > b then 
+    begin
+        if b > c then writeln('najmniejsza jest liczba c = ', c)
+        else  writeln('najmniejsza jest liczba b = ', b)
+    end
+    
+    else 
+    begin
+        if a < c then writeln('najmniejsza jest liczba a = ', a)
+        else  writeln('najmniejsza jest liczba c = ', c)
+    end
+end.
